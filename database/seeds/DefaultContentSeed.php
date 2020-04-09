6cfb3c2087fdd43a4b78184cf9832b44e3f29e97
Lars Åge Kamfjord <lars.age@kamfjord.org>
2020-04-09 15:26:11 +0200

    Changing seeding to put default content in DB

---
+++ database/seeds/DefaultContentSeed.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class DefaultContentSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Seed Permissions assignable
+        Permission::firstOrCreate(array('name' => 'roles'));
+        Permission::firstOrCreate(array('name' => 'events'));
+    }
+}
