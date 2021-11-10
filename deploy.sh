2c731097ac37d35f60883a8560e0aedc14bec44f
Lars Åge Kamfjord <lars.age@kamfjord.org>
2021-11-10 18:22:07 +0100

    Added some logging to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+DEPLOYLOG=storage/logs/deploy.log
+echo "Running git pull" >> $DEPLOYLOG
 git pull
-
+echo "Running composer install" >> $DEPLOYLOG
 composer install --no-interaction --no-dev --prefer-dist
-
+echo "Running migration" >> $DEPLOYLOG
 php artisan migrate --force
 
 
